ff2d92120ce5823e5973bb77bbfeec4dd9f83f29
Andrey Nikitchenko <shihki@inbox.ru>
2026-05-22 00:57:24 +0300

    Update BotConfig.java

--- src/main/java/ru/university/quizbot/config/BotConfig.java
+++ src/main/java/ru/university/quizbot/config/BotConfig.java
@@ -1,8 +1,8 @@
 package ru.university.quizbot.config;
 
 public class BotConfig {
-    public static final String BOT_USERNAME = "YourBotName";
-    public static final String BOT_TOKEN = "8992115253:AAG3puv2E8UaKPsyMyvUa-gX_4f76WT4q0g";
+    public static final String BOT_USERNAME = "Lab6Java";
+    public static final String BOT_TOKEN = "Токен_Сюда";
 
 
     public static final String LOG_FILE_PATH = "bot.log";
